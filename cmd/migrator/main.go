6adf3a751ea3cf27c169cfd274f88d4c3f84567d
romssc <xoticdollarsign@outlook.com>
2025-05-29 22:43:57 +0300

    refactor: add required imports

--- cmd/migrator/main.go
+++ cmd/migrator/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 
 	"getcitation/internal/utils"
 	"getcitation/internal/utils/config"

80ff049bbd48a3c88a02e4b62b1370988ae65df4
romssc <xoticdollarsign@outlook.com>
2025-05-29 22:29:45 +0300

    feat: complete migrator

--- cmd/migrator/main.go
+++ cmd/migrator/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/golang-migrate/migrate/v4"
+
+	"getcitation/internal/utils"
+	"getcitation/internal/utils/config"
+)
+
+const (
+	directionUp   = "up"
+	directionDown = "down"
+)
+
+func main() {
+	config, err := config.New()
+	if err != nil {
+		panic(err)
+	}
+
+	conn := utils.BuildPostgreSQLDSN(config)
+
+	m, err := migrate.New("file://"+config.MigrationsPath, conn)
+	if err != nil {
+		panic(err)
+	}
+
+	switch config.MigrationsDirection {
+	case directionUp:
+		err := m.Up()
+		if err != nil {
+			panic(err)
+		}
+
+	case directionDown:
+		err := m.Down()
+		if err != nil {
+			panic(err)
+		}
+
+	default:
+		panic("unknown direction")
+	}
+
+	fmt.Println("миграция завершена")
 }

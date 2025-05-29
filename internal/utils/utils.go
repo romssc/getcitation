43ce9fc50d23477190d94dd04c305837c031fa8f
romssc <xoticdollarsign@outlook.com>
2025-05-29 16:04:56 +0300

    feat: create utility func for opening log files

---
+++ internal/utils/utils.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func GetLogFile(path string) (*os.File, error) {
+	const op = "utils.GetLogFile()"
+
+	err := os.MkdirAll(filepath.Dir(path), 0777)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	return f, nil
+}
